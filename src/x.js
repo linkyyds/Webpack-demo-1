711509cafc13b1584fb192af40661de128ac2c35
link <1483166365@qq.com>
2023-01-11 18:41:35 +0800

    引入scss备份

--- src/x.js
+++ src/x.js
@@ -1,2 +1,3 @@
-import css from "./x.css";
 export default "xxx";
+import "./x.scss";
+import "./y.less";
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,4 +13,19 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
