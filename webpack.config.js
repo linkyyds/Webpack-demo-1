2d6cbfee483286e9174f7573baaa7526f4ec18a1
link <1483166365@qq.com>
2023-01-11 15:15:45 +0800

    使用了webpack-dev--save 来实时预览

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: { static: "./dist" },
   entry: "./src/index.js",
   output: {
     filename: "index.[contenthash].js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "我是title",
+      title: "Development",
       template: "src/assets/index.html",
     }),
   ],
