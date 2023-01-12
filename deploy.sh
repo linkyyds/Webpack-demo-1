8fc8f7394e9e985555ce94262beb6dbcbf89031e
link <1483166365@qq.com>
2023-01-12 14:33:31 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ mv dist/* ./ &&
 rm -rf dist &&
 git add . &&
 git commit -m 'update' &&
-git push --set-upstream origin gh-pages
+git push --set-upstream origin gh-pages &&
+git checkout - 
+# - 代表上一个分支
